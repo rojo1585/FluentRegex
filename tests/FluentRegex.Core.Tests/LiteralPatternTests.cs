d54b6185d5aabb021dfe9c7ab66ac5829581ae16
jorge.mendoza <jorge.mendoza@exodus.com.mx>
2026-07-27 19:24:16 -0600

    Move ConversionTests to separate file with namespace

--- tests/FluentRegex.Core.Tests/LiteralPatternTests.cs
+++ tests/FluentRegex.Core.Tests/LiteralPatternTests.cs
@@ -43,30 +43,6 @@ public class LiteralPatternTests
 
 
 
-public class ConversionTests
-{
-    [Fact]
-    public void ImplicitConversion_ToString()
-    {
-        string expr = Pattern.Literal("test");
-        Assert.Equal("test", expr);
-    }
-
-    [Fact]
-    public void ImplicitConversion_ToRegex()
-    {
-        Regex regex = Pattern.Digit().Repeat(3);
-        Assert.True(regex.IsMatch("123"));
-        Assert.False(regex.IsMatch("ab"));
-    }
-
-    [Fact]
-    public void ToString_ReturnsExpression()
-    {
-        var pattern = Pattern.Letter().OneOrMore();
-        Assert.Equal(pattern.Expression, pattern.ToString());
-    }
-}
 
 public class UsageMethodTests
 {
